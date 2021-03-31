ebca6ed9df9e6562878b10ee08204566a12cad47
SoharevSt <devsoharev@yandex.by>
2021-03-31 12:07:01 +0300

    3. Объединить два отсортированных массива

---
+++ src/ru/job4j/array/Merge.java
@@ -0,0 +1,22 @@
+package ru.job4j.array;
+
+/*
+Даны два отсортированных по возрастанию массива. Как без сортировки их объединить в третий массив?
+*/
+
+public class Merge {
+    public static int[] merge(int[] left, int[] right) {
+        int[] rsl = new int[left.length + right.length];
+        int i = 0, j = 0, k = 0;
+        while (i < left.length && j < right.length) {
+            rsl[k++] = left[i] < right[j] ? left[i++] : right[j++];
+        }
+        while (i < left.length) {
+            rsl[k++] = left[i++];
+        }
+        while (j < right.length) {
+            rsl[k++] = right[j++];
+        }
+        return rsl;
+    }
+}
